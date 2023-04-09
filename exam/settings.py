723b43812bc769ea8c45b75c6f4e020b25219852
taconi <igor.taconi@protonmail.com>
2023-04-08 22:09:26 -0300

    feat: salva os dados no banco de dados

--- exam/settings.py
+++ exam/settings.py
@@ -5,6 +5,12 @@ NEWSPIDER_MODULE = "exam.spiders"
 
 ROBOTSTXT_OBEY = False
 
+ITEM_PIPELINES = {
+    "exam.pipelines.ExamPipeline": 300,
+}
+
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+MONGO_URI = "mongodb://root:root@localhost:27017/"
